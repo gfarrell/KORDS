84f96111238329f4d9bff8ef73502a79e6021def
Gideon Farrell <me@gideonfarrell.co.uk>
2011-12-08 01:24:15 +0000

    Added basic appcontroler

---
+++ source/software/site/app/Controller/AppController.php
@@ -0,0 +1,22 @@
+<?php
+/*
+
+	AppController Base Controller
+	------------------------------
+
+	@file 		AppController.php
+	@author 	Gideon Farrell <me@gideonfarrell.co.uk>
+
+	Copyright (c) 2011 Gideon Farrell <http://www.gideonfarrell.co.uk>
+
+*/
+
+App::uses('Controller', 'Controller');
+
+class AppController extends Controller {
+	var $components = array('Raven', 'Session', 'Security');
+	var $helpers = array('Session', 'Html', 'Form');
+	
+	function isAuthorised() {}
+}
+?>

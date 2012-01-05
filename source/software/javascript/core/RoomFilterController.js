3f80466831e842c1b94d1053675bf8ecf5e96480
Gideon Farrell <me@gideonfarrell.co.uk>
2012-01-05 00:21:02 +0000

    Added RoomFilterController javascript class (needs creating)

---
+++ source/software/javascript/core/RoomFilterController.js
@@ -0,0 +1,13 @@
+/*
+
+	RoomFilterController Class
+	--------------------------
+
+	@file 		RoomFilterController.js
+	@author 	Gideon Farrell <me@gideonfarrell.co.uk>
+
+	Copyright (c) 2011 Gideon Farrell <http://www.gideonfarrell.co.uk>
+
+*/
+
+var RoomFilterController = new Class({});

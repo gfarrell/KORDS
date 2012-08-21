f44dd7c77df5bebeec6a4b7e88fc600dec6499f9
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-21 17:01:56 +0100

    Started defining a room model

---
+++ webroot/js/Model/Room.js
@@ -0,0 +1,19 @@
+/*
+    Model:Room
+    ----------
+
+    @file     Room.js
+    @package  Kords/Model
+    @author   Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+define(
+    ['Backbone/relational'],
+    function(Backbone) {
+        var Room = Backbone.Model.extend({
+            defaults: {},
+            initialize: {},
+            validate: {}
+        });
+    }    
+);

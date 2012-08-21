ec1890192cb0e374ef53197fdf87141f9fb88b36
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-21 02:07:21 +0100

    We are no longer using any cake plugins (js handled by require, css by livereload)

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -63,7 +63,3 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
-CakePlugin::load(array(
-    'Json',
-    'ResourcesController' => array('routes'=>true)
-));

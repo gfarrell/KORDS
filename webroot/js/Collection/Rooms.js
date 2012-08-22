6b4bb563a937ae18671b90dfec6e392abb61a0de
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-22 16:48:13 +0100

    Created a Rooms collection with a filter function for filtering by number

---
+++ webroot/js/Collection/Rooms.js
@@ -0,0 +1,38 @@
+/*
+    Collection:Rooms
+    ----------------
+
+    @file     Rooms.js
+    @package  Kords/Collection
+    @author   Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+define(
+    ['backbone', 'Model/Room'],
+    function(Backbone, Room) {
+        var RoomsCollection = Backbone.Collection.extend({
+            model: Room,
+            url: '/json/rooms',
+
+            filterByNumber: function(query, exact) {
+                exact = !!exact;                                        // Make sure it's a boolean
+                var regex = new RegExp(query, 'gi'),                    // Instantiate Regex obj (for non-exact matching)
+                    filt;                                               // The function we're going to use to filter
+
+                if(exact) {
+                    filt = function(room) {
+                        return (room.get('number') == query);           // Standard equality
+                    };
+                } else {
+                    filt = function(room) {
+                        return room.get('number').search(regex) != -1;   // str.search -1 if string is not found
+                    };
+                }
+
+                return this.filter(filt);                                // Run built-in _ method filter()
+            }
+        });
+
+        return RoomsCollection;
+    }
+);

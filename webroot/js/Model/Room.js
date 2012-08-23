c0b015245964a8c479255f5bffffc4c4ca4b1508
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-23 02:25:04 +0100

    Fixed related model problems and added full parse function
    Basically relations needed relatedModel to be the model class not a string as per the documentation

--- webroot/js/Model/Room.js
+++ webroot/js/Model/Room.js
@@ -8,8 +8,8 @@
  */
 
 define(
-    ['Backbone/relational'],
-    function(Backbone) {
+    ['Backbone/relational', 'Model/Location', 'Model/RentBand', 'Model/TenantType', 'Model/Comment', 'Model/RoomImage'],
+    function(Backbone, Location, RentBand, TenantType, Comment, RoomImage) {
         var Room = Backbone.RelationalModel.extend({
             url:            '/json/rooms/',    // API URL for rooms
 
@@ -19,23 +19,38 @@ define(
                 // ---------
                 {// Room belongsTo Location
                     type:               Backbone.HasOne,
-                    relatedModel:       'Location',
-                    key:                'Location'
+                    relatedModel:       Location,
+                    key:                'Location',
+                    includeInJSON:      'id'
+                },
+                {// Room belongsTo RentBand
+                    type:               Backbone.HasOne,
+                    relatedModel:       RentBand,
+                    key:                'RentBand',
+                    includeInJSON:      'id'
+                },
+                {// Room belongsTo TenantType
+                    type:               Backbone.HasOne,
+                    relatedModel:       TenantType,
+                    key:                'TenantType',
+                    includeInJSON:      'id'
                 },
 
                 // HasMany / HasAndBelongsToMany
                 // -----------------------------
                 {// Room hasMany Comment
                     type:               Backbone.HasMany,
-                    relatedModel:       'Comment',
+                    relatedModel:       Comment,
                     key:                'Comment',
-                    collectionType:     'CommentsCollection'
+                    //collectionType:     'CommentsCollection',
+                    includeInJSON:      'id'
                 },
-                {// Room hasAndBelongsToMany RoomImage
+                {// Room hasMany RoomImage
                     type:               Backbone.HasMany,
-                    relatedModel:       'RoomImage',
-                    key:                'RoomImages',
-                    collectionType:     'CommentsCollection'
+                    relatedModel:       RoomImage,
+                    key:                'RoomImage',
+                    //collectionType:     'RoomImagesCollection',
+                    includeInJSON:      'id'
                 }
             ],
 
@@ -44,7 +59,28 @@ define(
             initialize:     function() {},
 
             parse: function(response) {
-                return response.Room;
+                /* Response structure from Cake:
+                        {
+                            Room: { ... room attributes ... },
+                            Location: { ... location attributes ... },
+                            RentBand: { ... rent band attributes ... },
+
+                            --- NOT IN INDEX ---
+                            Comment: [ ... array of comments ... ], 
+                            RoomImage: [ ... array of roomimages ... ],
+                            TenantType: { ... tenant type attributes ... }
+                        }
+                 */
+                
+                var room = response.Room;
+
+                delete response.Room;
+
+                Object.each(response, function(item, key) {
+                    room[key] = item;
+                });
+
+                return room;
             }
         });
 

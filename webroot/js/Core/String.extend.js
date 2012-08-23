3079685b646aad73d83a80ce3df213cbe0e38f26
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-23 01:51:52 +0100

    Added decent camelcasing unlike Mootools' poor excuse for camelCase()

--- webroot/js/Core/String.extend.js
+++ webroot/js/Core/String.extend.js
@@ -4,6 +4,9 @@ define(
         return String.implement({
             humanise: function() {
                 return this.replace(/([\s\-_]+)/g, ' ').replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase().capitalize();
+            },
+            camelise: function() {
+                return this.replace(/(\w+)([\s\-_]+)(\w+)/g, function(whole, word1, blah, word2) { return word1 + word2.capitalize(); });
             }
         });
     }

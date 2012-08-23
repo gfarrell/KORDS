bef1298842745de3c5df03122fc5dc702f2811cf
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-23 14:14:36 +0100

    Created the Filter on init

--- webroot/js/View/Rooms/Index.js
+++ webroot/js/View/Rooms/Index.js
@@ -8,16 +8,44 @@
  */
 
 define(
-    ['backbone', 'text!Template/Rooms/Filter.html', 'View/Helper/Form'],
-    function(Backbone, FilterControl, filter_html, FormHelper) {
+    ['Mootools/core', 'jquery', 'underscore', 'backbone', 'text!Template/Rooms/Filter.html', 'View/Helper/Form', 'Collection/Locations', 'Collection/RentBands'],
+    function(_Mootools, $, _, Backbone, filter_html, FormHelper, LocationsCollection, RentBandsCollection) {
         var RoomsIndexView = Backbone.View.extend({
             tagName: 'div',
 
             initialize: function(opts) {
                 this.__nc = opts.__nc;
-
+                
                 // Set up a side filter
+                // We need to template the html (filter_html) and pass in the helper
+                // Along with some data that we're going to get now...
+                this.Locations = new LocationsCollection();
+                this.RentBands = new RentBandsCollection();
 
+                this.Locations.fetch();
+                this.RentBands.fetch();
+
+                var filter_temp = _.template(filter_html),
+                    filter_form = new FormHelper('Filter', {
+                            'inputDefaults': {
+                                'options': {
+                                    1: 'Yes',
+                                    0: 'No'
+                                },
+                                'class': 'filter'
+                            }
+                        });
+
+                this.$filter = new Element('div', {
+                    'class':    'sidebar left',
+                    'html':     filter_temp({
+                                    'Form':         filter_form,
+                                    'locations':    this.Locations.list(),
+                                    'rent_bands':   this.RentBands.list()
+                                })
+                });
+
+                $(this.$filter).appendTo(this.$el);
             },
 
             render: function() {

d9e7d33f14806fd0e181d8dfb0b4aacc3ce876cc
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-23 14:10:15 +0100

    Created a basic LocationsCollection

---
+++ webroot/js/Collection/Locations.js
@@ -0,0 +1,20 @@
+/*
+    Collection:Locations
+    --------------------
+
+    @file       Locations.js
+    @package    Kords/Collection
+    @author     Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+define(
+    ['Collection/AppCollection', 'Model/Location'],
+    function(AppCollection, Location) {
+        var LocationsCollection = AppCollection.extend({
+            model: Location,
+            url: '/json/locations'
+        });
+
+        return LocationsCollection;
+    }
+)

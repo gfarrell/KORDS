a0ea65c4aebc413ab28f118988f94c0f73e2ade9
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-24 01:48:56 +0100

    Added generic parse function

--- webroot/js/Model/AppModel.js
+++ webroot/js/Model/AppModel.js
@@ -11,7 +11,45 @@ define(
     ['Kords', 'Backbone/relational'],
     function(Kords) {
         Kords.AppModel = Backbone.RelationalModel.extend({
-            displayAttribute: 'name'
+            name:             'AppModel',
+            url:              '/json/models',
+            displayAttribute: 'name',
+
+            parse: function(response) {
+                // The way CakePHP gives us models is:
+                // [
+                //     {
+                //         Model: {},
+                //         RelatedModels: [],
+                //         RelatedModel: {},
+                //         ...
+                //     },
+                //     ...
+                // ]
+                //
+                // But Backbone wants something slightly different:
+                // [
+                //      {
+                //          id: ...,
+                //          attrs: ...,
+                //          ...
+                //          RelatedModels: [],
+                //          RelatedModel: {}
+                //      }
+                // ]
+                // 
+                // Thus our parse function will use the name attribute to get the core model data
+                // and add in the rest as relational
+                
+                var data = response[this.name];
+                delete response[this.name];
+
+                Object.each(response, function(obj, key) {
+                    data[key] = obj;
+                });
+
+                return data;
+            }
         });
 
         return Kords.AppModel;

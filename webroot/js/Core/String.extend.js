639758f7861be9bf719ec9a4cb840a52f7aa8455
Gideon Farrell <me@gideonfarrell.co.uk>
2012-08-31 20:36:02 +0100

    Lazy matching on camelise to make sure it does all the camel casing

--- webroot/js/Core/String.extend.js
+++ webroot/js/Core/String.extend.js
@@ -6,7 +6,7 @@ define(
                 return this.replace(/([\s\-_]+)/g, ' ').replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase().capitalize();
             },
             camelise: function() {
-                return this.replace(/(\w+)([\s\-_]+)(\w+)/g, function(whole, word1, blah, word2) { return word1 + word2.capitalize(); });
+                return this.replace(/(\w+?)([\s\-_]+?)(\w+?)/g, function(whole, word1, blah, word2) { return word1 + word2.capitalize(); });
             },
             underscore: function() {
                 return this.hyphenate().replace(/^\-/, '').replace('-', '_');

515b6341e9d85c8ea7cb19421f98b32f6e303a2c
Gideon Farrell <me@gideonfarrell.co.uk>
2012-09-01 03:37:41 +0100

    Added skeletal Display view

---
+++ webroot/js/View/Rooms/Display.js
@@ -0,0 +1,28 @@
+/*
+    View:Display
+    ------------
+
+    The view for /rooms/id/view.
+
+    @file       Display.js
+    @package    js/View/Rooms
+    @author     Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+define(
+    ['View/Kords', 'Model/Room', 'text!Template/Rooms/Display.html'],
+    function(KordsView, Room, display_html) {
+        var RoomDisplayView = KordsView.extend({
+            tagName: 'div',
+            className: 'room-display',
+            
+            templates: {
+                'main': display_html
+            },
+
+            initialize: function(opts) {
+                
+            }
+        });
+    }
+);
